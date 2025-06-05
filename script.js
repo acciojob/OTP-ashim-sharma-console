227a795625c886e84217c01d00717240c71d8e14
ashim-sharma-console <ashimsharma49@gmail.com>
2025-06-06 01:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
+
 const inputs = document.querySelectorAll('.code');
 
-inputs[0].focus();
 
 inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {
-    const value = input.value;
+  
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
 
-    // Only allow digits
-    if (!/^[0-9]$/.test(value)) {
+  
+    if (value.match(/[0-9]/)) {
+      
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+     
       input.value = '';
-      return;
-    }
-
-    // Move to next input if current has a digit
-    if (value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
     }
   });
+
 
   input.addEventListener('keydown', (e) => {
     if (e.key === 'Backspace') {
       if (input.value === '') {
         if (index > 0) {
           inputs[index - 1].focus();
-          inputs[index - 1].value = '';
         }
-      } else {
-        input.value = '';
       }
-    } else if (e.key >= 0 && e.key <= 9) {
-      // Allow only digit keys
-    } else if (e.key !== 'Tab') {
-      e.preventDefault();
     }
   });
+
+ 
+  if (index === 0) {
+    input.focus();
+  }
 });
